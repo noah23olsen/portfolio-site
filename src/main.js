65d4246f1e6861be579fad71b846b10d90cef827
noah23olsen <noah23olsen@gmail.com>
2023-11-09 09:53:12 -0500

    adding vercel analytics take 2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router/index.js';
-import { inject } from '@cercel/analytics';
+import { inject } from '@vercel/analytics';
 
 inject();
 createApp(App).use(router).mount('#app')
